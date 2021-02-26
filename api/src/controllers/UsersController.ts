3f1fd0e7654bb1380dfc23c1b4f642c57aacd18c
jgabriel1 <josegabriel@det.ufc.br>
2021-02-26 15:58:11 -0300

    Added input validation for user.

--- api/src/controllers/UsersController.ts
+++ api/src/controllers/UsersController.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 export class UsersController {
   async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({
+        error: err,
+      });
+    }
 
     const usersRepository = UsersRepository.getRepository();
 
